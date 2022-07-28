12fbb9fe32970cbe41176d528b2cfccf5e8972f0
Glenn Nguyo <glennnguyo1000@gmail.com>
2022-07-28 12:12:48 +0300

    Api

--- football-app/src/Component/Leagues.js
+++ football-app/src/Component/Leagues.js
@@ -1,9 +1,21 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 function Leagues(){
+        const [data, setData] = useState([]);
+      
+        useEffect(() => {
+          axios
+            .get("https://api-football-standings.azharimm.site/leagues")
+            .then((response) => {
+              console.log(response.data.data);
+              setData(response.data.data);
+            })
+            .catch((err) => console.log(err));
+        }, [])
     return(
-        <div className="leagues"></div>
+        <div className="leagues">
+        </div>
     )
-}
+            }
 
 export default Leagues;
