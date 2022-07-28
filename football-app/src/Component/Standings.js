0e0ea55d68ff568c72288f60b102713dc985e75e
Glenn Nguyo <glennnguyo1000@gmail.com>
2022-07-28 12:22:33 +0300

    Standings-get

--- football-app/src/Component/Standings.js
+++ football-app/src/Component/Standings.js
@@ -1,6 +1,25 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 function Standings(){
+        const [selectedLeague, setSelectedLeague] = useState("eng.1");
+        const [selectedYear, setSelectedYear] = useState("2021");
+        const [data, setData] = useState([]);
+        const [loading, setLoading] = useState(false);
+      
+        useEffect(() => {
+          setLoading(true);
+          setData([]);
+          axios
+            .get(
+              `https://api-football-standings.azharimm.site/leagues/${selectedLeague}/standings?season=${selectedYear}`
+            )
+            .then((res) => {
+              console.log(res.data.data.standings);
+              setData(res.data.data.standings);
+            })
+            .catch((err) => console.log(err))
+            .finally(() => setLoading(false));
+        }, [selectedYear, selectedLeague]);
     return(
         <div className="standings"></div>
     )
